39e5bfb4fe42bbc7a62908835a5ef1bada207899
Tomás Murphy <tomas.murphy.tm@gmail.com>
2022-08-02 11:07:37 -0300

    primer commit

---
+++ js/experiencias.js
@@ -0,0 +1,348 @@
+document.addEventListener('DOMContentLoaded', () => {
+const herbario = `        <div class="row marMob">
+<div class="col-12 fondoImg text-md-start">
+  <h2>
+    <i class="bi bi-bug"></i>
+    Herbario doméstico
+  </h2>
+  <hr />
+  <h3 class="text-end fs-6">
+    "Naturaleza no es lo que vemos <br />
+    la montaña, el poniente la ardilla, el eclipse, el abejorro <br />
+    no, naturaleza es el cielo <br />
+    naturaleza es lo que oímos <br />
+    el bobolink, el mar el trueno, el grillo <br />
+    no, naturaleza es la armonía <br />
+    naturaleza es lo que sabemos <br />
+    no tenemos arte para decirlo <br />
+    tan impotente es nuestra sabiduría <br />
+    para tanta simplicidad” <br /><br />
+    Emily Dickinson (1863)
+  </h3>
+  <div class="row">
+    <img src="./img/herbario_domestico/herbario_domestico_1.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/herbario_domestico/herbario_domestico_3.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/herbario_domestico/herbario_domestico_4.jpg" class="col-md-4 img-fluid" alt="" />
+  </div>
+  </div>  
+  <div class="col-12 fondoImg text-md-start">
+
+  <p>
+    Herbario doméstico es un proyecto colectivo de fotografía
+    analógica centrado en la naturaleza como tema y como insumo. <br><br>
+    Nos convocan los entornos naturales y por ello hemos elegido
+    fotografiar plantas de nuestros espacios cotidianos en sintonía
+    con la experimentación en el proceso de revelado. ¿No es
+    increíble, por ejemplo, ver la imagen de una menta en tu jardín y
+    que eso sea posible gracias a su composición química? Enamoradas
+    de la fotografía analógica, y preocupadas por el impacto que
+    nuestras acciones tienen sobre el ambiente venimos investigando
+    acerca de reveladores orgánicos provenientes de plantas para
+    películas blanco y negro. <br><br> 
+    Hemos experimentado procesos de revelado
+    a base de café, té negro, whisky, menta, tomillo, romero, orégano,
+    entre otros. Estas mezclas biodegradables reemplazan al revelador
+    químico fotográfico y sus efectos contaminantes. Creemos, además,
+    que los resultados y los efectos que obtenemos en la imagen se
+    enriquecen gracias a estas técnicas y estamos sorprendidas por la
+    nobleza y la calidad que estos materiales nos ofrecen.
+  </p>
+  <div class="row">
+    <img src="./img/herbario_domestico/herbario_domestico_2.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/herbario_domestico/herbario_domestico_6.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/herbario_domestico/herbario_domestico_7.jpg" class="col-md-4 img-fluid" alt="" />
+ 
+    <img src="./img/herbario_domestico/herbario_domestico_5.jpg" class="col-md-6 img-fluid" alt="" />
+    <img src="./img/herbario_domestico/herbario_domestico_8.jpg" class="col-md-3 img-fluid" alt="" />
+    <img src="./img/herbario_domestico/herbario_domestico_9.jpg" class="col-md-3 img-fluid" alt="" />
+  </div>
+  <p>
+    <i
+      >*Este proyecto fue seleccionado para ser mostrado en el CC Cine
+      Lumiere de la ciudad de Rosario durante la convocatoria 2022.
+    </i>
+  </p>
+</div>
+</div>
+`,
+jacinto = `<div class="row marMob">
+<div class="col-12 fondoImg text-md-start">
+  <h2>
+    <i class="bi bi-droplet"></i>
+    Jacinto de agua común
+  </h2>
+  <hr />
+  </div>  
+  <div class="col-12 fondoImg text-md-start">
+
+  <p>
+    Jacinto de agua común es un proyecto de fotografía sin cámara y sin químicos. La intención es independizarnos de las cámaras fotográficas tanto de 35mm y de formato medio como de materiales tóxicos contaminantes. Este proyecto de investigación plantea la realización de fotografías estenopeicas en papel y reveladas a través de fórmulas con plantas de menta, orégano, rosa mosqueta y cortezas de árboles locales. Nuestros reveladores reemplazan a los tradicionales químicos industriales para fotografía en blanco y negro.  <br><br>
+    Nos interesa la fotografía estenopeica porque queremos lograr imágenes que trasciendan la noción de paisaje tradicional. Las cámaras están realizadas con materiales reciclados, a partir de envases de cartón y latas. Hacer fotografía química / analógica, pero sin utilizar materiales contaminantes nos permite conservar la estética de este tipo de fotografía a la vez que agrega elementos brindados por el entorno natural.  Cada planta le otorga a la fotografía su propio color en el papel. A diferencia del revelado de película en negativo, que conserva una escala de grises única.  <br><br>
+    El objetivo general del proyecto es poder encontrar los tiempos de exposición y las especificidades de los revelados naturales para lograr resultados óptimos sin que sea necesario el uso de químicos. Si bien hemos realizado pruebas con buenos resultados aún debemos seguir probando tiempos y disoluciones de las plantas y de este modo llegar a los resultados definitivos. <br><br>
+    Para realizar estas fotos recorremos las ciudades de Santa Fe y Paraná. Lugares que se vinculan con nuestra práctica artística. Los cruces entre territorio y paisaje plantean el eje por el cual se desarrolla este proyecto y que tiene como particularidad la convivencia con los lugares en los cuales vivimos y trabajamos.<br><br>
+      
+  </p>
+  <div class="row">
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_1.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_2.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_3.jpg" class="col-md-4 img-fluid" alt="" />
+ 
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_4.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_5.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_6.jpg" class="col-md-4 img-fluid" alt="" />
+  </div>
+</div>
+</div>`,
+miscelaneas = `<div class="row marMob">
+<div class="col-12 fondoImg text-md-start">
+  <h2>
+    <i class="bi bi-cloud"></i>
+        Misceláneas
+  </h2>
+  <hr />
+  <h3 class="text-end fs-6">
+    Reveladas con orégano y rosa mosqueta
+  </h3>  
+</div>  
+  <div class="col-12 fondoImg text-md-start">
+
+  <div class="row">
+    <img src="./img/miscelaneas/miscelaneas_1.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_2.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_3.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_4.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_5.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_6.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_7.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_8.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/miscelaneas/miscelaneas_9.jpg" class="col-md-4 img-fluid" alt="" />
+      
+  </div>
+</div>
+</div>
+`,
+fotogramas = `<div class="row marMob">
+<div class="col-12 fondoImg text-md-start">
+  <h2>
+    <i class="bi bi-flower2"></i>
+        Fotogramas botánicos
+  </h2>
+  <hr />
+  <h3 class="text-end fs-6">
+    Revelados con papa
+  </h3>
+  
+  </div>  
+  <div class="col-12 fondoImg text-md-start">
+
+  <div class="row">
+    <img src="./img/fotogramas/fotogramas_botanicos_1.jpg" class="col-md-6 img-fluid" alt="" />
+    <img src="./img/fotogramas/fotogramas_botanicos_3.jpg" class="col-md-6 img-fluid" alt="" />
+      
+  </div>
+  <div class="row">
+    <div class="col-md-4"></div>
+    <img src="./img/fotogramas/fotogramas_botanicos_2.jpg" class="col-md-4 img-fluid" alt="" />
+      
+  </div>
+</div>
+</div>
+`,
+talleres = `<div class="row marMob">
+<div class="col-12 fondoImg text-md-start">
+  <h2>
+    <i class="bi bi-book-half"></i>
+        Talleres y residencias
+  </h2>
+  <hr />
+  </div>  
+  <div class="col-12 fondoImg text-md-start">
+
+  <div class="row">
+    <img src="./img/talleres/talleres_y_residencias_1.jpg" class="col-md-6 img-fluid" alt="" />
+    <img src="./img/talleres/talleres_y_residencias_2.jpg" class="col-md-6 img-fluid" alt="" />
+   </div>
+   <div class="row">
+
+   <img src="./img/talleres/talleres_y_residencias_3.jpg" class="col-md-6 img-fluid" alt="" />
+    <img src="./img/talleres/talleres_y_residencias_4.jpg" class="col-md-6 img-fluid" alt="" />
+      
+  </div>
+  <div class="row">
+    <img src="./img/talleres/talleres_y_residencias_5.jpg" class="col-md-4 img-fluid" alt="" />
+    <img src="./img/talleres/talleres_y_residencias_6.jpg" class="col-md-8 img-fluid" alt="" />
+      
+  </div>
+</div>
+</div>
+`,
+inicio = `<div class="row marMob">
+<div class="cajaDeCajas">
+  <div class="cajaImagen">
+    
+<img src="./img/herbario_domestico/herbario_domestico_1.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">  
+<img src="./img/herbario_domestico/herbario_domestico_3.jpg" class=" img-fluid" alt="" />
+</div><div class="cajaImagen">    
+<img src="./img/herbario_domestico/herbario_domestico_4.jpg" class=" img-fluid" alt="" />
+</div><div class="cajaImagen">    
+<img src="./img/herbario_domestico/herbario_domestico_2.jpg" class=" img-fluid" alt="" />
+</div><div class="cajaImagen">    
+<img src="./img/herbario_domestico/herbario_domestico_6.jpg" class=" img-fluid" alt="" />
+</div><div class="cajaImagen">    
+<img src="./img/herbario_domestico/herbario_domestico_7.jpg" class=" img-fluid" alt="" />
+</div><div class="cajaImagen">
+    <img src="./img/herbario_domestico/herbario_domestico_5.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/herbario_domestico/herbario_domestico_8.jpg" class=" img-fluid" alt="" />
+    
+  </div><div class="cajaImagen">
+    <img src="./img/herbario_domestico/herbario_domestico_9.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_1.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_2.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_3.jpg" class=" img-fluid" alt="" />
+ 
+  </div><div class="cajaImagen">
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_4.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_5.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/jacinto_de_agua_comun/jacinto_de_agua_comun_6.jpg" class=" img-fluid" alt="" />
+
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_1.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_2.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_3.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_4.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_5.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_6.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_7.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_8.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/miscelaneas/miscelaneas_9.jpg" class=" img-fluid" alt="" />  
+  </div><div class="cajaImagen">
+    <img src="./img/fotogramas/fotogramas_botanicos_1.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/fotogramas/fotogramas_botanicos_3.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/fotogramas/fotogramas_botanicos_2.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/talleres/talleres_y_residencias_1.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/talleres/talleres_y_residencias_2.jpg" class=" img-fluid" alt="" />
+   
+  </div><div class="cajaImagen">
+    <img src="./img/talleres/talleres_y_residencias_3.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/talleres/talleres_y_residencias_4.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/talleres/talleres_y_residencias_5.jpg" class=" img-fluid" alt="" />
+  </div><div class="cajaImagen">
+    <img src="./img/talleres/talleres_y_residencias_6.jpg" class=" img-fluid" alt="" />
+</div>
+</div>
+</div>`;
+const 
+btnHerbario = document.querySelector('#btnHerbario'),
+btnJacinto = document.querySelector('#btnJacinto'),
+btnMiscelaneas = document.querySelector('#btnMiscelaneas'),
+btnFotogramas = document.querySelector('#btnFotogramas'),
+btnTalleres = document.querySelector('#btnTalleres');
+        
+const crearItems = document.querySelector('#crearItems'); 
+const crearMain = document.createElement('main');
+crearMain.setAttribute("id", "items")
+crearItems.appendChild(crearMain);
+
+function cargarHtml(filtro) {
+    var borrar = document.getElementById("items");
+    padre = borrar.parentNode;
+    padre.removeChild(borrar);
+    const crearMain = document.createElement('main');
+    crearMain.setAttribute("id", "items")
+    crearItems.appendChild(crearMain);
+
+    switch (filtro) {
+        case 1:
+            var btnActivo = herbario
+            btnHerbario.classList.add("activo");
+            btnJacinto.classList.remove("activo");
+            btnMiscelaneas.classList.remove("activo");
+            btnFotogramas.classList.remove("activo");
+            btnTalleres.classList.remove("activo");
+            break;
+        case 2:
+            var btnActivo = jacinto
+            btnHerbario.classList.remove("activo");
+            btnJacinto.classList.add("activo");
+            btnMiscelaneas.classList.remove("activo");
+            btnFotogramas.classList.remove("activo");
+            btnTalleres.classList.remove("activo");
+            break;
+        case 3:
+            var btnActivo = miscelaneas 
+            btnHerbario.classList.remove("activo");
+            btnJacinto.classList.remove("activo");
+            btnMiscelaneas.classList.add("activo");
+            btnFotogramas.classList.remove("activo");
+            btnTalleres.classList.remove("activo");
+            break;
+        case 4:
+            var btnActivo = fotogramas
+            btnHerbario.classList.remove("activo");
+            btnJacinto.classList.remove("activo");
+            btnMiscelaneas.classList.remove("activo");
+            btnFotogramas.classList.add("activo");
+            btnTalleres.classList.remove("activo");
+            break;
+        case 5:
+            var btnActivo = talleres
+            btnHerbario.classList.remove("activo");
+            btnJacinto.classList.remove("activo");
+            btnMiscelaneas.classList.remove("activo");
+            btnFotogramas.classList.remove("activo");
+            btnTalleres.classList.add("activo");
+            break;    
+        default:
+            var btnActivo = inicio
+            btnHerbario.classList.remove("activo");
+            btnJacinto.classList.remove("activo");
+            btnMiscelaneas.classList.remove("activo");
+            btnFotogramas.classList.remove("activo");
+            btnTalleres.classList.remove("activo");
+            break;
+    }
+
+            const divHtml = document.createElement('div');
+            divHtml.innerHTML = btnActivo;
+            crearMain.appendChild(divHtml);
+    }
+
+    btnHerbario.addEventListener("click", () => {
+        cargarHtml(1);
+    });
+    btnJacinto.addEventListener("click", () => {
+        cargarHtml(2);
+    });
+    btnMiscelaneas.addEventListener("click", () => {
+        cargarHtml(3);
+    });
+    btnFotogramas.addEventListener("click", () => {
+        cargarHtml(4);
+    });
+    btnTalleres.addEventListener("click", () => {
+        cargarHtml(5);
+    });
+    cargarHtml()
+    
+});
